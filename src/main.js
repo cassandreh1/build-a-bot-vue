306f04c8ed37ef7138ddfcb6d0e5b77404152478
cassandreh1 <cassandreh01@gmail.com>
2020-06-16 19:05:16 -0400

    adding global currency filter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import Vue from 'vue';
 import {
-  BootstrapVue, BootstrapVueIcons, BIcon, BIconBagFill,
+  BootstrapVue, BootstrapVueIcons, BIcon, BIconBagFill, BIconX,
 } from 'bootstrap-vue';
 import 'bootstrap';
 import App from './App.vue';
@@ -11,6 +11,14 @@ Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.component('BIcon', BIcon);
 Vue.use('BIconBagFill', BIconBagFill);
+Vue.use('BIconX', BIconX);
+
+Vue.filter('currency', (value) => {
+  if (!value) return '';
+  const formatedValue = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(value);
+  return formatedValue;
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
